2ff5f3cbd3b94633201b01066f669c4f59bedcee
felipeferraz <felipe.ferraz@viannasempre.com.br>
2018-03-15 19:40:15 -0300

    WSFisica

---
+++ WSExercicios/WSFisica.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WSFisica.asmx.cs" Class="WSExercicios.WSFisica" %>
---
+++ WSExercicios/WSFisica.asmx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WSExercicios
+{
+    /// <summary>
+    /// Summary description for WSFisica
+    /// </summary>
+    [WebService(Namespace = "http://WSFisica.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WSFisica : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public double VelocidadeMedia(double Distancia, int tempo)
+        {
+            return Distancia / tempo;
+        }
+
+        [WebMethod]
+        public double Aceleracao(double Velocidade, int tempo)
+        {
+            return Velocidade / tempo;
+        }
+
+        [WebMethod]
+        public double Forca(double massa, int aceleracao)
+        {
+            return Distancia / tempo;
+        }
+    }
+}
+//Velocidade Média entrará com a Distancia e tempo: V = S /  T;
+//Aceleração(entradas: Velocidade e Tempo) : A = V / T
+//Força: (entradas: massa e aceleração): F = m* a;
