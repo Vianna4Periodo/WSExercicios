6fa8c4d90ea1070100ac323111ee75cf74f1aed6
felipeferrazdeveloper <felipeferrazdeveloper@users.noreply.github.com>
2018-03-15 18:49:22 -0300

    WSBancario

---
+++ WSExercicios/WSBancario.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WSBancario.asmx.cs" Class="WSExercicios.WSBancario" %>
---
+++ WSExercicios/WSBancario.asmx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WSExercicios
+{
+    /// <summary>
+    /// Summary description for WSBancario
+    /// </summary>
+    [WebService(Namespace = "http://WSBancario.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WSBancario : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string NovoCliente(string nome, string cpf, double saldo_inicial)
+        {
+            if(saldo_inicial > 0 && !String.IsNullOrEmpty(cpf) && !String.IsNullOrEmpty(nome))
+            {
+                var conta = new Random();
+                return "Cliente: " + nome + " - " + cpf + " --> Conta: " + conta.Next(000000,999999).ToString();
+            }
+            else
+            {
+                throw new Exception("Nome e CPF são obrigatórios o saldo nao pode ser menor que zero.");
+            }
+        }
+    }
+}
